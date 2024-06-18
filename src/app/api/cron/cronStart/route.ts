78a4988eb760a0e12210908cd2f1e51e52969503
saurabhrk92 <saurabhrk@gmail.com>
2024-06-17 22:03:45 -0700

    Updated to run cron via GET

--- src/app/api/cron/cronStart/route.ts
+++ src/app/api/cron/cronStart/route.ts
@@ -1,14 +1,14 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import { scheduleCronJobs } from '@/utils/scheduleCron'; // Update the path as needed
 
 let jobsScheduled = false;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest) {
     if (!jobsScheduled) {
         await scheduleCronJobs().catch((error) => {
             console.error('Error scheduling cron jobs:', error);
         });
         jobsScheduled = true;
     }
-    res.status(200).json({ message: 'Cron jobs scheduled' });
+    return NextResponse.json({ message: 'Cron jobs scheduled' });
 }

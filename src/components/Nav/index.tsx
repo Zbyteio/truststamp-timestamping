7fa569bf1c539a4069b9d523fff9f65c7a9f894c
Saurabh Radhakrishnan <saurabh@genovatic.com>
2024-06-24 08:35:58 -0700

    Updated nav to add recovery and remove home

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -7,7 +7,7 @@ export default function Nav() {
 		<nav className={styles.nav}>
 			<Image src={logoTruststamp} alt="TrustStamp Logo" width={60} height={60} />
 			<ul className={styles.links}>
-				<li><a className={styles.link} href="/">Home</a></li>
+				<li><a className={styles.link} href="/recovery">Recovery</a></li>
 				<li><a className={styles.link} href="/setup">Setup</a></li>
 				<li><a className={styles.link} href="/feature">Features</a></li>
 				<li><a className={styles.link} href="/dashboard">Dashboard</a></li>
